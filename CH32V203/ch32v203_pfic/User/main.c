b0e806bbcbe0ad9b1af457a4ab77f93a759e4b75
SteveLRojas <looserr2@illinois.edu>
2024-08-24 14:37:21 +0200

    Added PFIC and AFIO libraries for CH32V203

---
+++ CH32V203/ch32v203_pfic/User/main.c
@@ -0,0 +1,100 @@
+#include "ch32v20x.h"
+#include "ch32v203_core.h"
+#include "debug.h"
+#include "fifo.h"
+#include "ch32v203_uart.h"
+#include "ch32v203_gpio.h"
+#include "ch32v203_spi.h"
+#include "ch32v203_rcc.h"
+#include "ch32v203_timer.h"
+
+//Pins:
+// LED3 = PA8
+// TXD = PA9
+// RXD = PA10
+// UDM = PA11
+// UDP = PA12
+// SPI1_NCS = PA15
+// SPI1_SCK = PB3
+// SPI1_MISO = PB4
+// SPI1_MOSI = PB5
+// LED2 = PC13
+// LED1 = PC14
+// LED0 = PC15
+
+void TIM1_UP_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+
+void TIM1_UP_IRQHandler(void)
+{
+	gpio_toggle_pin(GPIOC, GPIO_PIN_15);
+	gpio_toggle_pin(GPIOC, GPIO_PIN_14);
+	gpio_toggle_pin(GPIOA, GPIO_PIN_8);
+    /* clear status */
+    timer_clear_interrupt_flag(TIM1);
+}
+
+int main(void)
+{
+	//NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	rcc_apb2_clk_enable(RCC_AFIOEN | RCC_IOPAEN | RCC_IOPBEN | RCC_IOPCEN | RCC_TIM1EN | RCC_SPI1EN | RCC_USART1EN);
+	rcc_apb1_clk_enable(RCC_TIM2EN);
+
+	gpio_set_mode(GPIOA, GPIO_DIR_SPD_OUT_50MHZ | GPIO_MODE_AFIO_PP, GPIO_PIN_9);
+	gpio_set_mode(GPIOA, GPIO_DIR_SPD_IN | GPIO_MODE_FLOAT_IN, GPIO_PIN_10);
+	gpio_set_mode(GPIOA, GPIO_DIR_SPD_OUT_50MHZ | GPIO_MODE_PP_OUT, GPIO_PIN_8 | GPIO_PIN_15);
+	gpio_set_mode(GPIOB, GPIO_DIR_SPD_OUT_50MHZ | GPIO_MODE_AFIO_PP, GPIO_PIN_3 | GPIO_PIN_5);
+	gpio_set_mode(GPIOB, GPIO_DIR_SPD_IN | GPIO_MODE_PULL_IN, GPIO_PIN_4);
+	gpio_set_mode(GPIOC, GPIO_DIR_SPD_OUT_2MHZ | GPIO_MODE_PP_OUT, GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
+	gpio_set_pin(GPIOB, GPIO_PIN_4);
+
+	Delay_Init();
+	Delay_Ms(1);
+	uart_init(USART1, 115200);
+	core_enable_irq(USART1_IRQn);
+
+	printf("SYSCLK: %u\n", rcc_compute_sysclk_freq());
+	printf("HCLK: %u\n", rcc_compute_hclk_freq());
+	printf("PCLK1: %u\n", rcc_compute_pclk1_freq());
+	printf("PCLK2: %u\n", rcc_compute_pclk2_freq());
+	printf("ADCCLK: %u\n", rcc_compute_adcclk());
+
+	// blink the led once
+	gpio_set_pin(GPIOC, GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
+	gpio_set_pin(GPIOA, GPIO_PIN_8);
+	Delay_Ms(100);
+	gpio_clear_pin(GPIOC, GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
+	gpio_set_pin(GPIOA, GPIO_PIN_8);
+	Delay_Ms(100);
+	gpio_write_pin(GPIOC, GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, 1);
+	Delay_Ms(100);
+
+	timer_init(TIM1, 96000000 / 10000 - 1, 1000 - 1);	//100us time scale, 100ms period
+	timer_init(TIM2, 96000000 / 1000000 - 1, 1000 - 1);	//1us time scale, 1ms period
+	timer_enable_interrupt(TIM1);
+	core_enable_irq(TIM1_UP_IRQn);
+	timer_start(TIM1);
+
+	printf("Unicorn\n");
+
+	uint32_t iteration_count;
+
+	while(1)
+	{
+		//test long delay
+		iteration_count = 1000;
+		timer_set_period(TIM2, 1000 - 1);	//1ms period
+		do
+		{
+			timer_long_delay(TIM2, 2);	//2ms delay
+			gpio_toggle_pin(GPIOC, GPIO_PIN_13);
+		} while(--iteration_count);
+
+		//test short delay
+		iteration_count = 10000;
+		do
+		{
+			timer_short_delay(TIM2, 200);	//200us delay
+			gpio_toggle_pin(GPIOC, GPIO_PIN_13);
+		} while(--iteration_count);
+	}
+}
---
+++ CH32V203/ch32v203_pfic/obj/User/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../User/ch32v20x_it.c \
+../User/main.c 
+
+OBJS += \
+./User/ch32v20x_it.o \
+./User/main.o 
+
+C_DEPS += \
+./User/ch32v20x_it.d \
+./User/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+User/%.o: ../User/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU RISC-V Cross C Compiler'
+	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -msmall-data-limit=8 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -I"C:\Users\Steve\mrs_community_workspace\ch32v203_pfic\Debug" -I"C:\Users\Steve\mrs_community_workspace\ch32v203_pfic\user_libs" -I"C:\Users\Steve\mrs_community_workspace\ch32v203_pfic\User" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
