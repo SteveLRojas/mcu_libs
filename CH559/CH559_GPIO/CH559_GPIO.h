f61cce7a36ec8bb690b0d7c7986767c26d2ff04c
SteveLRojas <looserr2@illinois.edu>
2025-04-26 10:41:12 +0200

    Added GPIO interrupt support to the CH559 GPIO library

--- CH559/CH559_GPIO/CH559_GPIO.h
+++ CH559/CH559_GPIO/CH559_GPIO.h
@@ -1,6 +1,9 @@
 #ifndef _CH559_GPIO_H_
 #define _CH559_GPIO_H_
 
+#define USE_PORT_FUNCS 0
+#define USE_GPIO_INTERRUPTS 1
+
 #define GPIO_PORT_MODE_PP 0
 #define GPIO_PORT_MODE_OC 1
 
@@ -24,6 +27,7 @@
 #define GPIO_PORT_2 2
 #define GPIO_PORT_3 3
 #define GPIO_PORT_4 4
+// HINT: Port 5 has the USB and reset pins, for GPIO purposes it is an input only port.
 
 #define GPIO_PIN_0 0x01
 #define GPIO_PIN_1 0x02
@@ -34,6 +38,27 @@
 #define GPIO_PIN_6 0x40
 #define GPIO_PIN_7 0x80
 
+#if USE_GPIO_INTERRUPTS
+#define GPIO_INT_MODE_LEVEL	0x00
+#define GPIO_INT_MODE_EDGE	0x80
+
+#define GPIO_INT_RXD1_FALL	0x40
+#define GPIO_INT_P5_5_RISE	0x20
+#define GPIO_INT_P1_4_FALL	0x10
+#define GPIO_INT_P0_3_FALL	0x08
+#define GPIO_INT_P5_7_RISE	0x04
+#define GPIO_INT_P4_1_FALL	0x02
+#define GPIO_INT_RXD0_FALL	0x01
+
+extern void (*gpio_callback)(void);
+
+#define gpio_global_interrupt_enable() (IE_GPIO = 1)
+#define gpio_global_interrupt_disable() (IE_GPIO = 0)
+#define gpio_set_interrupt_mode(mode) (GPIO_IE = (GPIO_IE & ~bIE_IO_EDGE) | (mode))
+#define gpio_enable_interrupts(interrupts) (GPIO_IE |= (interrupts))
+#define gpio_disable_interrupts(interrupts) (GPIO_IE &= ~(interrupts))
+#endif
+
 // HINT: for ports 0 to 3
 #define gpio_set_port_mode(mode, port) (PORT_CFG = (PORT_CFG & ~(0x01 << (port))) | ((mode) << (port)))
 #define gpio_set_port_strength(strength, port) (PORT_CFG = (PORT_CFG & ~(0x10 << (port))) | ((strength) << ((port) + 4)))
@@ -42,7 +67,7 @@
 #define gpio_set_p4_strength(strength) (P4_CFG = (P4_CFG & ~0x04) | ((strength) << 2))
 
 void gpio_set_pin_mode(UINT8 mode, UINT8 port, UINT8 pins);
-#ifdef USE_PORT_FUNCS
+#if USE_PORT_FUNCS
 void gpio_write_port(UINT8 port, UINT8 write_data);
 UINT8 gpio_read_port(UINT8 port);
 #endif
--- CH559/CH559_GPIO/main.c
+++ CH559/CH559_GPIO/main.c
@@ -1,27 +1,39 @@
 #include "CH559.H"
-#include "DEBUG.H"
+#include "CH559_RCC.h"
 #include "CH559_GPIO.h"
+
+void on_gpio_int(void)
+{
+	gpio_toggle_pin(GPIO_PORT_1, GPIO_PIN_4);
+}
 
 int main()
 {
-	CfgFsys();	//CH559 clock selection configuration
+	rcc_set_clk_freq(RCC_CLK_FREQ_12M);
 
 	gpio_set_port_mode(GPIO_PORT_MODE_PP, GPIO_PORT_1);
 	gpio_set_port_strength(GPIO_PORT_STRENGTH_20, GPIO_PORT_1);
 	gpio_set_pin_mode(GPIO_MODE_OUTPUT_PP, GPIO_PORT_1, GPIO_PIN_4 | GPIO_PIN_5);
+	gpio_set_port_mode(GPIO_PORT_MODE_PP, GPIO_PORT_4);
+	gpio_set_pin_mode(GPIO_MODE_INPUT, GPIO_PORT_4, GPIO_PIN_1);
+	
+	gpio_callback = on_gpio_int;
+	gpio_set_interrupt_mode(GPIO_INT_MODE_EDGE);
+	gpio_enable_interrupts(GPIO_INT_P4_1_FALL);
+	gpio_global_interrupt_enable();
+	EA = 1;	//enable interupts
+	E_DIS = 0;
 	
 	// blink led once
 	gpio_set_pin(GPIO_PORT_1, GPIO_PIN_4);
 	gpio_set_pin(GPIO_PORT_1, GPIO_PIN_5);
-	mDelaymS(250);
+	rcc_delay_ms(250);
 	gpio_clear_pin(GPIO_PORT_1, GPIO_PIN_4);
 	gpio_clear_pin(GPIO_PORT_1, GPIO_PIN_5);
 
 	while(TRUE)
 	{
-		mDelaymS(125);
-		gpio_write_pin(GPIO_PORT_1, GPIO_PIN_5, gpio_read_pin(GPIO_PORT_1, GPIO_PIN_4));
-		gpio_toggle_pin(GPIO_PORT_1, GPIO_PIN_4);
+		rcc_delay_ms(125);
+		gpio_toggle_pin(GPIO_PORT_1, GPIO_PIN_5);
 	}
 }
-
